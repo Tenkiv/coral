76ab9aed90d53082e848445a1efff54b0af3bbde
Zack Juhasz <zjuhasz@tenkiv.com>
2016-09-03 23:07:44 -0700

    Adding a blocking concurrency mode to unitmut.

--- src/main/kotlin/org/tenkiv/coral/unimut.kt
+++ src/main/kotlin/org/tenkiv/coral/unimut.kt
@@ -1,27 +1,58 @@
 package org.tenkiv.coral
 
+import java.util.concurrent.locks.Condition
 import java.util.concurrent.locks.ReadWriteLock
 import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.concurrent.withLock
 import kotlin.reflect.KProperty
 
 
-class AlreadySetException : UnsupportedOperationException {
-    constructor() : super()
-    constructor(message: String) : super(message)
-    constructor(cause: Throwable) : super(cause)
-    constructor(message: String, cause: Throwable) : super(message, cause)
+/**
+ * Creates a delegate for properties that you want to mutate / set exactly one time but may not have the necessary
+ * information to do so at compile time.
+ *
+ * @param onSet is called after setting the backing field to the new value.
+ * @param onGet is called before returning the value of the property.
+ */
+fun <T : Any> unimut(concurrencyMode: UnimutConcurrencyMode = UnimutConcurrencyMode.PUBLICATION,
+                     onGet: ((T?) -> Unit)? = null,
+                     onSet: ((T) -> Unit)? = null): UniMutDelegate<T> {
+    when (concurrencyMode) {
+        UnimutConcurrencyMode.BLOCKING -> return BlockingUniMutDelegate(onSet, onGet)
+        UnimutConcurrencyMode.SYNCHRONIZED -> return SynchronisedUniMutDelegate(onSet, onGet)
+        UnimutConcurrencyMode.PUBLICATION -> return PublicationSafeUnitMutDelegate(onSet, onGet)
+        UnimutConcurrencyMode.NONE -> return UniMutDelegate(onSet, onGet)
+    }
 }
 
-class KeyNotFoundException : Throwable {
-    constructor() : super()
-    constructor(message: String) : super(message)
-    constructor(cause: Throwable) : super(cause)
-    constructor(message: String, cause: Throwable) : super(message, cause)
+/**
+ * Creates a Synchronised unimut and allows to use a custom ReadWriteLock. This is only recommended for advanced users
+ * who have a specific reason for wanting to do this.
+ *
+ * @param blockMode with blockMode enabled the thread will be blocked until this property is set when you try to
+ * access it.
+ */
+fun <T : Any> unimut(blockMode: Boolean,
+                     lock: ReadWriteLock,
+                     onGet: ((T?) -> Unit)? = null,
+                     onSet: ((T) -> Unit)? = null): UniMutDelegate<T> =
+        if (blockMode)
+            BlockingUniMutDelegate(onSet, onGet, lock)
+        else
+            SynchronisedUniMutDelegate(onSet, onGet, lock)
+
+enum class UnimutConcurrencyMode {
+    /**
+     * Blocks the thread attempting to get the property until the property is set.
+     */
+    BLOCKING,
+    SYNCHRONIZED,
+    PUBLICATION,
+    NONE
 }
 
 open class UniMutDelegate<T : Any> internal constructor(protected open var onSet: ((T) -> Unit)?,
-                                                        private val onGet: ((T?) -> Unit)?) {
+                                                        protected val onGet: ((T?) -> Unit)?) {
     open var value: T? = null
 
     open operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
@@ -46,7 +77,7 @@ private class PublicationSafeUnitMutDelegate<T : Any>(@Volatile override var onS
 }
 
 private class SynchronisedUniMutDelegate<T : Any>(
-        @Volatile override var onSet: ((T) -> Unit)?,
+        onSet: ((T) -> Unit)?,
         onGet: ((T?) -> Unit)?,
         private val lock: ReadWriteLock = ReentrantReadWriteLock()) : UniMutDelegate<T>(onSet, onGet) {
 
@@ -59,28 +90,52 @@ private class SynchronisedUniMutDelegate<T : Any>(
     }
 }
 
-/**
- * Creates a delegate for properties that you want to mutate / set exactly one time but may not have the necessary
- * information to do so at compile time.
- *
- * @param onSet is called after setting the backing field to the new value.
- * @param onGet is called before returning the value of the property.
- */
-fun <T : Any> unimut(threadSafetyMode: LazyThreadSafetyMode = LazyThreadSafetyMode.PUBLICATION,
-                     onGet: ((T?) -> Unit)? = null,
-                     onSet: ((T) -> Unit)? = null): UniMutDelegate<T> {
-    when (threadSafetyMode) {
-        LazyThreadSafetyMode.SYNCHRONIZED -> return SynchronisedUniMutDelegate(onSet, onGet)
-        LazyThreadSafetyMode.PUBLICATION -> return PublicationSafeUnitMutDelegate(onSet, onGet)
-        LazyThreadSafetyMode.NONE -> return UniMutDelegate(onSet, onGet)
+private class BlockingUniMutDelegate<T : Any>(onSet: ((T) -> Unit)?,
+                                              onGet: ((T?) -> Unit)?,
+                                              private val lock: ReadWriteLock = ReentrantReadWriteLock()) :
+        UniMutDelegate<T>(onSet, onGet) {
+    var setCondition: Condition? = lock.writeLock().newCondition()
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        lock.readLock().withLock {
+            if (value != null) {
+                onGet?.invoke(value)
+                return value!!
+            }
+        }
+        lock.writeLock().withLock {
+            //Double check that nothing else got the lock and set this while we were upgrading from a read lock to
+            //a write lock
+            if (value != null) {
+                onGet?.invoke(value)
+                return value!!
+            } else {
+                setCondition?.await()
+                onGet?.invoke(value)
+                return value!!
+            }
+        }
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        lock.writeLock().withLock {
+            super.setValue(thisRef, property, value)
+            setCondition?.signalAll()
+            setCondition = null
+        }
     }
 }
 
-/**
- * Creates a Synchronised unimut and allows to use a custom ReadWriteLock. This is only recommended for advanced users
- * who have a specific reason for wanting to do this.
- */
-fun <T : Any> unimut(lock: ReadWriteLock,
-                     onGet: ((T?) -> Unit)? = null,
-                     onSet: ((T) -> Unit)? = null): UniMutDelegate<T> =
-        SynchronisedUniMutDelegate(onSet, onGet, lock)
+class AlreadySetException : UnsupportedOperationException {
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(cause: Throwable) : super(cause)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+}
+
+class KeyNotFoundException : Throwable {
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(cause: Throwable) : super(cause)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+}
