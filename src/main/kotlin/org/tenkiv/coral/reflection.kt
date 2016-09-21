7d46a79f939caac48de15a3dcd2e9d0e97ffe4d7
Zack Juhasz <zjuhasz@tenkiv.com>
2016-09-21 16:23:15 -0700

    Refactoring.

--- src/main/kotlin/org/tenkiv/coral/reflection.kt
+++ src/main/kotlin/org/tenkiv/coral/reflection.kt
@@ -3,8 +3,8 @@ package org.tenkiv.coral
 import kotlin.reflect.KClass
 
 /**
- * Checks to see if one Kotlin type conforms to another. Will return the same result as if the 'is' keyword were
- * used to check an instance of the class against a KClass.
+ * Checks to see if one Kotlin type conforms to another. Will return the same result as if the 'is' operator were
+ * used to check an instance of the class against a type.
  */
 infix fun <T : Any, C : Any> KClass<T>.can(comparate: KClass<C>) =
         comparate.java.isAssignableFrom(this.java)
