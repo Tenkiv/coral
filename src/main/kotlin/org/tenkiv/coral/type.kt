7f93567a5f56bf653468a68d5e7f7baab2a828d8
Zack Juhasz <zjuhasz@tenkiv.com>
2016-10-15 01:47:23 -0700

    Made extension functions for creating ValueInstant.

--- src/main/kotlin/org/tenkiv/coral/type.kt
+++ src/main/kotlin/org/tenkiv/coral/type.kt
@@ -2,18 +2,20 @@ package org.tenkiv.coral
 
 import java.time.Instant
 
-interface ValueInstant<out T : Any> {
+fun <T> T.at(instant: Instant = Instant.now()): ValueInstant<T> = BasicValueInstant(this, instant)
+
+interface ValueInstant<out T> {
     val value: T
     val instant: Instant
 
     companion object {
-        fun <T : Any> of(value: T, instant: Instant = Instant.now()): ValueInstant<T> =
+        fun <T> of(value: T, instant: Instant = Instant.now()): ValueInstant<T> =
                 BasicValueInstant(value, instant)
     }
 }
 
-private data class BasicValueInstant<out T : Any>(override val value: T,
-                                                  override val instant: Instant = Instant.now()) :
+private data class BasicValueInstant<out T>(override val value: T,
+                                            override val instant: Instant = Instant.now()) :
         ValueInstant<T>
 
 //TODO: Add a data class that implements both ValueInstant and MeasurementCQ
