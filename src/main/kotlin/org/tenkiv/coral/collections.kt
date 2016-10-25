19ef15827157e7b280931602ab3df5aa293f6c03
Zack Juhasz <zjuhasz@tenkiv.com>
2016-10-24 23:44:28 -0700

    Added findRation extension to Collections.

--- src/main/kotlin/org/tenkiv/coral/collections.kt
+++ src/main/kotlin/org/tenkiv/coral/collections.kt
@@ -25,8 +25,23 @@ inline fun <E> Collection<E>.average(getValue: (E) -> Double, condition: (E) -> 
     return totalValue / totalElements
 }
 
+inline fun <E> Collection<E>.findRatio(of: (E) -> Double, to: (E) -> Double): Double {
+    var firstTotal = 0.0
+    var secondTotal = 0.0
+
+    forEach {
+        firstTotal += of(it)
+        secondTotal += to(it)
+    }
+
+    return firstTotal / secondTotal
+}
+
 inline fun <E> SynchronisedCollection<E>.average(getValue: (E) -> Double) =
         lock.readLock().withLock { unSyncedCollection.average(getValue) }
 
 inline fun <E> SynchronisedCollection<E>.average(getValue: (E) -> Double, condition: (E) -> Boolean) =
         lock.readLock().withLock { unSyncedCollection.average(getValue, condition) }
+
+inline fun <E> SynchronisedCollection<E>.findRatio(of: (E) -> Double, to: (E) -> Double) =
+        lock.readLock().withLock { unSyncedCollection.findRatio(of, to) }
