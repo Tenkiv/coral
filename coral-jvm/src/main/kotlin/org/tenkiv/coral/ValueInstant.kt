f451b20effb0a1281130d1dced339b3b412537c0
Zack Juhasz <zjuhasz@tenkiv.org>
2018-07-27 15:55:19 +0100

    * Deprecated ValueInstant companion invoke. * Added getDataInRange() for Iterable of ValueInstant

--- coral-jvm/src/main/kotlin/org/tenkiv/coral/ValueInstant.kt
+++ coral-jvm/src/main/kotlin/org/tenkiv/coral/ValueInstant.kt
@@ -3,9 +3,12 @@ package org.tenkiv.coral
 import java.time.Instant
 
 
-infix fun <T> T.at(instant: Instant): ValueInstant<T> = ValueInstant(this, instant)
+infix fun <T> T.at(instant: Instant): ValueInstant<T> = BasicValueInstant(this, instant)
 
-fun <T> T.now(): ValueInstant<T> = ValueInstant(this, Instant.now())
+fun <T> T.now(): ValueInstant<T> = BasicValueInstant(this, Instant.now())
+
+fun <T> Iterable<ValueInstant<T>>.getDataInRange(instantRange: ClosedRange<Instant>): List<ValueInstant<T>> =
+    this.filter { it.instant in instantRange }
 
 interface ValueInstant<out T> {
     val value: T
@@ -15,6 +18,7 @@ interface ValueInstant<out T> {
     operator fun component2() = instant
 
     companion object {
+        @Deprecated("Use at(Instant) function instead", ReplaceWith("at(Instant)"))
         operator fun <T> invoke(value: T, instant: Instant = Instant.now()): ValueInstant<T> =
             BasicValueInstant(value, instant)
     }
